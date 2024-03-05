12d5c6f7c28aa2226c1fd8c94e5bc7c8972113c7
robsonstss <robsonsantosjose3210@gmail.com>
2024-03-05 11:29:59 -0300

    Funcionar botões

---
+++ app.js
@@ -0,0 +1,5 @@
+function mostrarTexto() {
+    let mensagem = document.getElementById('conteudo__entrada__texto').value;
+    document.getElementById('conteudo__saida__texto').value = mensagem;
+}
+
