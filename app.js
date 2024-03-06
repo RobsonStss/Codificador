eedbab043445c3be5bb251c57d82e34448f00906
robsonstss <robsonsantosjose3210@gmail.com>
2024-03-06 09:48:58 -0300

    desaparecer imagem

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const texto = [];
+const chavers = [e="nter", i="mes", a="i", o="ber", u="fat"]
+
 function mostrarTexto() {
-    let mensagem = document.getElementById('conteudo__entrada__texto').value;
-    document.getElementById('conteudo__saida__texto').value = mensagem;
+    var mensagem = document.getElementById('conteudo__entrada__texto').value;
+    texto.push(mensagem);
+    document.getElementById('conteudo__saida__texto').value = texto;
 }
 
+function desaparecerImagem() {
+    document.getElementById("conteudo__saida__texto").style.backgroundImage = "none";
+}
+
+function criptografar() {
+    if (mensagem.indexOf("a")) {
+        
+    }
+}
