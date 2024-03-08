49a25c8df4eb0e15a35ec8561009e1b89d0fd5fe
robsonstss <robsonsantosjose3210@gmail.com>
2024-03-08 11:24:04 -0300

    Fazer a descriptografia

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 // chaves = [e="nter", i="mes", a="i", o="ber", u="fat"];
 var textoEntrada = document.querySelector("#conteudo__entrada__texto");
-
-
+var resultadoCriptgrafado;
+var resultadoDescrip;
 
 function criptografar() {
-    var textoSaida = textoEntrada.value;
-    var resultadoCriptgrafado = textoSaida.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
-    console.log(resultadoCriptgrafado);
+    var textoSaidaCript = textoEntrada.value;
+    resultadoCriptgrafado = textoSaidaCript.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+}
+
+function descriptografar() {
+    var textoSaidaDescript = textoEntrada.value;
+    resultadoDescrip = textoSaidaDescript.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
 }
 
 function desaparecerImagem() {
     document.getElementById("conteudo__saida__texto").style.backgroundImage = "none";
 }
 
-function mostrarTexto() {
-    document.getElementById('conteudo__saida__texto').value = resultado;
+function mostrarTextoCrip() {
+    document.getElementById("conteudo__saida__texto").value = resultadoCriptgrafado;
 }
 
-function comportamento() {
+function mostrarTextoDescrip() {
+    document.getElementById("conteudo__saida__texto").value = resultadoDescrip;
+}
+
+function botaoCrip() {
     criptografar();
+    desaparecerImagem();
+    mostrarTextoCrip();
 }
 
+function botaoDescrip() {
+    descriptografar();
+    mostrarTextoDescrip();
+    desaparecerImagem();
+}
