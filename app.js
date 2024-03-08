e42c9c1f1ef85646fae4f512c02a1106ad20ff91
robsonstss <robsonsantosjose3210@gmail.com>
2024-03-08 09:54:31 -0300

    Fazer a criptografia

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
-var lista = [];
-// const chaves = [e="nter", i="mes", a="i", o="ber", u="fat"];
-var mensagem;
+// chaves = [e="nter", i="mes", a="i", o="ber", u="fat"];
+var textoEntrada = document.querySelector("#conteudo__entrada__texto");
 
-function pegarTexto() {
-    mensagem = document.getElementById('conteudo__entrada__texto').value;
-    lista.push(mensagem);
+
+
+function criptografar() {
+    var textoSaida = textoEntrada.value;
+    var resultadoCriptgrafado = textoSaida.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+    console.log(resultadoCriptgrafado);
 }
 
 function desaparecerImagem() {
     document.getElementById("conteudo__saida__texto").style.backgroundImage = "none";
 }
 
-function criptografar() {
-    if (mensagem.includes('a')) {
-        lista.push("i");
-    }
-    if (mensagem.includes('e')) {
-        lista.push("nter");
-    }
-}
-
-function embaralharTexto() {
-    let a = lista.length *  Math.random(lista);
-    console.log(lista);
-}
-
 function mostrarTexto() {
-    document.getElementById('conteudo__saida__texto').value = lista;
+    document.getElementById('conteudo__saida__texto').value = resultado;
 }
 
 function comportamento() {
-    pegarTexto();
     criptografar();
-    mostrarTexto();
-    desaparecerImagem();
-    embaralharTexto();
 }
 
