e9c8a1de489d0ed3c746bac70b32813db08d4959
robsonstss <robsonsantosjose3210@gmail.com>
2024-03-09 22:41:36 -0300

    Refatorar código

--- app.js
+++ app.js
@@ -1,43 +1,25 @@
-var textoEntrada = document.querySelector("#conteudo__entrada__texto");
+var textoInput = document.querySelector('#main__entrada__texto');
+var textOutput = document.querySelector('#main__saida__texto');
 var resultadoCriptgrafado;
-var resultadoDescrip;
+var resultadoDescriptografado;
 
 function criptografar() {
-    var textoSaidaCript = textoEntrada.value;
-    resultadoCriptgrafado = textoSaidaCript.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+    var texto = textoInput.value;
+    resultadoCriptografado = texto.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+
+    document.getElementById('main__saida').innerHTML = '<textarea name="name" id="main__saida__texto" placeholder="Nenhum texto encontrado">' + resultadoCriptografado +'</textarea>' + '<button class="botao-copiar" id="copiar" onclick="copiar()">Copiar</button>';
 }
 
 function descriptografar() {
-    var textoSaidaDescript = textoEntrada.value;
-    resultadoDescrip = textoSaidaDescript.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+    var texto = textoInput.value;
+    resultadoDescriptografado = texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('main__saida').innerHTML = '<textarea name="name" id="main__saida__texto" placeholder="Nenhum texto encontrado">' + resultadoDescriptografado +'</textarea>' + '<button class="botao-copiar" id="copiar" onclick="copiar()">Copiar</button>';
 }
 
-function desaparecerImagem() {
-    document.getElementById("conteudo__saida__texto").style.backgroundImage = "none";
-}
-
-function mostrarTextoCrip() {
-    document.getElementById("conteudo__saida__texto").value = resultadoCriptgrafado;
-}
-
-function mostrarTextoDescrip() {
-    document.getElementById("conteudo__saida__texto").value = resultadoDescrip;
-}
-
-function botaoCriptografar() {
-    criptografar();
-    desaparecerImagem();
-    mostrarTextoCrip();
-}
-
-function botaoDescriptografar() {
-    descriptografar();
-    mostrarTextoDescrip();
-    desaparecerImagem();
-}
-
-function copiarTexto() {
-    var copiarTexto = document.getElementById("conteudo__saida__texto");
+function copiar() {
+    var copiarTexto = document.getElementById("main__saida__texto");
     copiarTexto.select();
     navigator.clipboard.writeText(copiarTexto.value);
     alert(`Texto copiado ${copiarTexto.value}`);
